4654c199ea2dcd2f80fd57278b21a75b5b0fa204
KarnOrasai <karn_prasai@cmu.ac.th>
2024-01-26 15:21:01 +0700

    lab 1 & 2 prototype

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,23 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *xPtr = &a;
+	char *yPtr = &b;
+	int **zPtr = &xPtr;
 
-	
-	
+	cout << a <<" "<< b <<" "<<c<<" "<<xPtr<<" "<<&yPtr<<" "<<zPtr<<"\n";
+	c = 'F';
+	cout << &a <<" "<< &b <<" "<< &c <<" "<< &xPtr <<" "<<&yPtr<<" "<<&zPtr<<"\n";
+	*yPtr = 'W';
+	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<zPtr<<"\n";
+	*xPtr = 6;
+	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<zPtr<<"\n";
+	**zPtr = 7;
+	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<zPtr<<"\n";
 	return 0;
 }
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -2,6 +2,45 @@
 #include <algorithm>
 using namespace std;
 
+void sort(int *a1[],int *a2[])
+{
+	for(int i=0;i<*a[1];i++)
+		cout << *a[i];
+	/*
+	for(int i = 0;i<5;i++)
+	{
+		
+		if(*a1[i] > *a1[i+1])
+		{
+			for(int j = 0;j < i;j++)
+			{
+				int temp = *a1[j+1];
+				*a1[j+1] = *a1[j];
+				*a1[j] = temp;
+				
+			}
+		}
+		
+	}
+	for(int i = 10;i>5;i--)
+	{
+		
+		if(*a1[i] < *a1[i+1])
+		{
+			for(int j = 10;j > i;j++)
+			{
+				int temp = *a1[j];
+				*a1[j] = *a1[j+1];
+				*a1[j+1] = temp;
+				
+			}
+		}
+		
+	}
+	*/
+}
+
+
 int main(){
 	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
 	cout << "Before: ";
