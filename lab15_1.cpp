560f3710959bf1d96d56483405274571d14b1d72
Karn_Prasai <karn_prasai@cmu.ac.th>
2024-02-01 15:23:39 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -8,16 +8,17 @@ int main()
 	char &c = b;
 	int *xPtr = &a;
 	char *yPtr = &b;
-	int **zPtr = &xPtr;
+	void *zPtr = xPtr;
 
-	cout << a <<" "<< b <<" "<<c<<" "<<xPtr<<" "<<&yPtr<<" "<<zPtr<<"\n";
+	cout << a <<" "<< b <<" "<<c<<" "<<xPtr<<" "<<&yPtr<<" "<<static_cast<int*>(zPtr)<<"\n";
+	cout << &a <<" "<< static_cast<void*>(&b) <<" "<< static_cast<void*>(&c) <<" "<< static_cast<void*>(&xPtr) <<" "<<static_cast<void*>(&yPtr)<<" "<< zPtr <<"\n";
 	c = 'F';
-	cout << &a <<" "<< &b <<" "<< &c <<" "<< &xPtr <<" "<<&yPtr<<" "<<&zPtr<<"\n";
+	cout << a <<" "<< b <<" "<<c<<" "<<xPtr<<" "<<&yPtr<<" "<<static_cast<int*>(zPtr)<<"\n";
 	*yPtr = 'W';
-	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<zPtr<<"\n";
+	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<static_cast<int*>(zPtr)<<"\n";
 	*xPtr = 6;
-	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<zPtr<<"\n";
-	**zPtr = 7;
-	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<zPtr<<"\n";
+	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<static_cast<int*>(zPtr)<<"\n";
+	*static_cast<int*>(zPtr) = 7;
+	cout << a <<" "<< b <<" "<< c <<" "<< xPtr <<" "<<&yPtr<<" "<<static_cast<int*>(zPtr)<<"\n";
 	return 0;
 }
